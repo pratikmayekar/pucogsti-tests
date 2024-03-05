01a7ff163a65d2000eec8b74af798400cc8196d2
Pratik Mayekar <pratikmayekar4@gmail.com>
2024-03-05 19:07:08 +0100

    Added virtual keyboard for math task

--- src/Screens/MathTask.jsx
+++ src/Screens/MathTask.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import {
   MathCorrectDuration,
@@ -7,6 +7,8 @@ import {
   MathOperandMax,
   MathOperandMin,
 } from "../common/Config";
+import KeyboardReact from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 
 const Container = styled.div`
   height: 100vh;
@@ -25,10 +27,15 @@ const TaskDescDiv = styled.div`
 `;
 const TaskContentDiv = styled.div`
   width: 60vw;
-  height: 50vh;
+  height: 60vh;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  justify-content: space-evenly;
+`;
+
+const KeypadDiv = styled.div`
+  width: 40%;
 `;
 const EndButtonDiv = styled.div`
   width: 60vw;
@@ -46,8 +53,7 @@ const AnswerInput = styled.input`
   width: 100px;
 `;
 const CheckDiv = styled.div`
-  width: 70px;
-  margin-left: 10px;
+  height: 22px;
 `;
 const CorrectSpan = styled.span`
   color: green;
@@ -62,7 +68,7 @@ const TaskDesc =
   "This activity has been designed to activate the numerical/logical reasoning parts of your brain.";
 const MathOperations = ["+", "*", "-"];
 
-function MathTask({endTask}) {
+function MathTask({ endTask }) {
   const [disabled, setDisabled] = useState(false);
   const [answerVal, setAnswerVal] = useState("");
   const [operand1, setOperand1] = useState();
@@ -70,6 +76,7 @@ function MathTask({endTask}) {
   const [correctAnswer, setCorrectAnswer] = useState();
   const [checkVal, setCheckVal] = useState(null);
   const [currOperation, setCurrOperation] = useState(0);
+  const keyboard = useRef();
 
   useEffect(() => {
     setNewProblem();
@@ -90,20 +97,21 @@ function MathTask({endTask}) {
   const setNewProblem = () => {
     setCheckVal(null);
     setAnswerVal("");
+    keyboard.current.setInput("");
     let op1, op2, operation;
 
     let probType = Math.random() < 0.3;
-    
-    const randomIntFromInterval = (min, max) => { // min and max included 
-      return Math.floor(Math.random() * (max - min + 1) + min)
-    }    
 
-    if(probType) {
+    const randomIntFromInterval = (min, max) => {
+      // min and max included
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    if (probType) {
       op1 = randomIntFromInterval(MathOperandMin, MathOperandMax);
       op2 = randomIntFromInterval(MathOperandMin, MathOperandMax);
       operation = Math.floor(Math.random() * MathOperations.length);
-    }
-    else {
+    } else {
       op1 = randomIntFromInterval(MathHardOperandMin, MathOperandMax);
       op2 = randomIntFromInterval(MathHardOperandMin, MathOperandMax);
       operation = Math.floor(Math.random() * (MathOperations.length - 1)); // exclude subtraction
@@ -135,6 +143,28 @@ function MathTask({endTask}) {
     }
   };
 
+  const onChangeInput = (event) => {
+    const inputVal = event.target.value.replace(/\D/, "");
+    setAnswerVal(inputVal);
+    setCheckVal(null);
+    keyboard.current.setInput(inputVal);
+  };
+  const onVirtualKeyPress = (button) => {
+    switch (button) {
+      case "{enter}": {
+        checkAnswer();
+        break;
+      }
+      case "{bksp}": {
+        const newVal = answerVal.slice(0, -1);
+        setAnswerVal(newVal);
+        keyboard.current.setInput(newVal);
+        break;
+      }
+    }
+    button === "{enter}" && checkAnswer();
+  };
+
   return (
     <Container>
       <TitleDiv>{TaskTitle}</TitleDiv>
@@ -151,13 +181,10 @@ function MathTask({endTask}) {
           <AnswerInput
             type="text"
             value={answerVal}
-            onChange={(event) => {
-              setAnswerVal(event.target.value.replace(/\D/, ""));
-              setCheckVal(null);
+            onChange={onChangeInput}
+            onKeyDown={(event) => {
+              event.key === "Enter" && checkAnswer();
             }}
-            onKeyDown={(event) =>
-              event.key === "Enter" ? checkAnswer() : true
-            }
             disabled={disabled}
             autoFocus
           />
@@ -171,9 +198,29 @@ function MathTask({endTask}) {
             <></>
           )}
         </CheckDiv>
+        <KeypadDiv style={{ visibility: disabled ? "hidden" : "visible" }}>
+          <KeyboardReact
+            keyboardRef={(r) => (keyboard.current = r)}
+            onChange={(input) => setAnswerVal(input)}
+            onKeyPress={(button) => onVirtualKeyPress(button)}
+            layout={{
+              default: ["1 2 3", "4 5 6", "7 8 9", "{bksp} 0 {enter}"],
+            }}
+            display={{
+              "{enter}": "enter",
+              "{bksp}": "backspace",
+            }}
+            theme="hg-theme-default hg-layout-numeric numeric-theme"
+          />
+        </KeypadDiv>
       </TaskContentDiv>
       <EndButtonDiv>
-        <button style={{display: disabled ? 'inline-block' : 'none'}} onClick={() => endTask()}>End Experiment</button>
+        <button
+          style={{ display: disabled ? "inline-block" : "none" }}
+          onClick={() => endTask()}
+        >
+          End Experiment
+        </button>
       </EndButtonDiv>
     </Container>
   );
